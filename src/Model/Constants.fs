df109aa252f83b6dd659898f2a4ecb19a7e6aa72
David Sanromá <dsanroma@gmail.com>
2022-09-13 16:23:55 +0200

    Constantes con [<Literal>] después del let.

--- src/Model/Constants.fs
+++ src/Model/Constants.fs
@@ -2,20 +2,9 @@ namespace Model
 
 module Constants =
 
-    [<Literal>]
-    let COL_MAX_VAL = 4
-
-    [<Literal>]
-    let COL_IP = 0
-
-    [<Literal>]
-    let COL_NAME = 1
-
-    [<Literal>]
-    let COL_DESCRIPTION = 2
-
-    [<Literal>]
-    let COL_IP_COLOR_NAME = 3
-
-    [<Literal>]
-    let COL_IP_IS_ACTIVE = 4
+    let [<Literal>] COL_MAX_VAL = 4
+    let [<Literal>] COL_IP = 0
+    let [<Literal>] COL_NAME = 1
+    let [<Literal>] COL_DESCRIPTION = 2
+    let [<Literal>] COL_IP_COLOR_NAME = 3
+    let [<Literal>] COL_IP_IS_ACTIVE = 4
